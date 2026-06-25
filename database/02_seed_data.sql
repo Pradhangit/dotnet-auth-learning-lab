9a1dbdf1ee3684c89c74a59fd4c3beef59b6fed7
pratyush.pradhan <pratyush.pradhan.003@gmail.com>
2026-06-25 17:51:31 +0530

    Add seed data for departments and designations

---
+++ database/02_seed_data.sql
@@ -0,0 +1,20 @@
+Departments:
+- Software
+- IT Helpdesk
+- Marketing
+- Sales
+- Human Resources
+
+Designations:
+- Level 1, 2, 3, 4 designations department-wise
+
+Sample users:
+- Ground-level users
+- Level 2 users
+- Level 3 users
+- Level 4 users
+
+ReportingAuthorityId mapping:
+- Level 1 reports to Level 2
+- Level 2 reports to Level 3
+- Level 3 reports to Level 4
